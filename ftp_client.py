c88a658fd4d5a69355e48b494a835942289b61bf
Rizal Septiarakhman <rizalseptiarakhman@gmail.com>
2016-12-20 08:53:50 +0700

    Fixed minor bug in RETR & STOR
    Fixed

--- ftp_client.py
+++ ftp_client.py
@@ -32,43 +32,57 @@ try:
                     sys.exit(0)
                     break
                 if 'STOR' in msg:
-                    command, filename = msg.split(' ', 1)
-                    filename = filename.rstrip('\n')
-                    client_socket.send(filename)
-                    print '+ Status\t:\tUploading ' + filename
-                    b = os.path.getsize(filename)
-                    b = str(b)
-                    client_socket.send(b)
-                    b = int(b)
-                    with open(filename, 'rb') as f:
-                        data = ""
-                        while 1:
-                            baca = f.read(1024)
-                            data += baca
-                            if len(data) >= b:
-                                break
-                        client_socket.send(data)
-                    #time.sleep(1)
-                    pesan = client_socket.recv(1024)
-                    sys.stdout.write('# Response\t:\t' + pesan)
+                    if '500 Syntax error' in pesan:
+                        print 'Please reinput your command'
+                    else:
+                        command, filename = msg.split(' ', 1)
+                        filename = filename.rstrip('\n')
+                        client_socket.send(filename)
+                        print '+ Status\t:\tUploading ' + filename
+                        b = os.path.getsize(filename)
+                        b = str(b)
+                        client_socket.send(b)
+                        b = int(b)
+                        with open(filename, 'rb') as f:
+                            data = ""
+                            while 1:
+                                baca = f.read(1024)
+                                data += baca
+                                if len(data) >= b:
+                                    break
+                            client_socket.send(data)
+                        #time.sleep(1)
+                        pesan = client_socket.recv(1024)
+                        sys.stdout.write('# Response\t:\t' + pesan)
                 if 'RETR' in msg:
-                    command, filename = msg.split(' ', 1)
-                    filename = filename.rstrip('\n')
-                    client_socket.send(filename)
-                    print '+ Status\t:\tDownloading ' + filename
-                    size = client_socket.recv(1024)
-                    size = int(size)
-                    with open(filename, 'wb') as f:
-                        isi = ''
-                        while 1:
-                            dapet = client_socket.recv(1024)
-                            isi += dapet
-                            if len(isi) >= size:
-                                break
-                        f.write(isi)
-                    time.sleep(1)
-                    pesan = client_socket.recv(1024)
-                    sys.stdout.write('# Response\t:\t' + pesan)
+                    if '500 Syntax error' in pesan:
+                        print 'Please reinput your command'
+                    else:
+                        flag = ''
+                        command, filename = msg.split(' ', 1)
+                        filename = filename.rstrip('\n')
+                        client_socket.send(filename)
+                        print '+ Status\t:\tDownloading ' + filename
+                        size = client_socket.recv(1024)
+                        size = int(size)
+                        with open(filename, 'wb') as f:
+                            isi = ''
+                            while 1:
+                                dapet = client_socket.recv(1024)
+                                if '226 ' in dapet:
+                                    flag += dapet.split('226 ')[1]
+                                    isi += dapet.split('226 ')[0]
+                                else:
+                                    isi += dapet
+                                if len(isi) >= size:
+                                    break
+                            f.write(isi)
+                        #time.sleep(1)
+                        if flag:
+                            print '# Response\t:\t226 Transfer complete.\r\n'
+                        else:
+                            pesan = client_socket.recv(1024)
+                            sys.stdout.write('# Response\t:\t' + pesan)
 
                 if 'DELE' in msg:
                     command, filename = msg.split(' ', 1)
